b795463a022e1303bacc1f4c1de1a5443d2367ba
Junior <ipwnjianrong@gmail.com>
2020-11-23 13:00:02 +0800

    Streamlined code and removed redundant functions

--- script.js
+++ script.js
@@ -1,64 +1,31 @@
 let ticker = ''
-let price = ''
-let dcf = ''
-let counter = 0
+let price = 0
+let dcf = 0
 
 async function displayData() {
     ticker = document.getElementById("stockName").value.toUpperCase();
 
-    // get currentPrice data
-    getRequest(
-        'https://financialmodelingprep.com/api/v3/company/profile/' + ticker + "?apikey=c666ff95d41eef82744356cb28a0c041",
-        drawPriceOutput
-    );
+    // display current price
+    let profileUrl = 'https://financialmodelingprep.com/api/v3/company/profile/' + ticker + "?apikey=c666ff95d41eef82744356cb28a0c041"
+    const prof = (async () => {
+        const response = await fetch(profileUrl);
+        const json = await response.json();
 
-    // get DCF data
-    getRequest(
-        'https://financialmodelingprep.com/api/v3/discounted-cash-flow/' + ticker + "?apikey=c666ff95d41eef82744356cb28a0c041",
-        drawDCFOutput
-    );
+        price = json['profile']['price']
+        document.getElementById("currentPrice").innerHTML = price
+    })()
+
+    // display dcf
+    let dcfUrl = 'https://financialmodelingprep.com/api/v3/discounted-cash-flow/' + ticker + "?apikey=c666ff95d41eef82744356cb28a0c041"
+    const dcprof = (async () => {
+        const response = await fetch(dcfUrl);
+        const json = await response.json();
+
+        dcf = json[0]['dcf']
+        document.getElementById("DCF").innerHTML = Math.round(dcf * 100) / 100
+    })()
+
     await new Promise(r => setTimeout(r, 1000));
     let marginOfSafety = ((price - dcf) / price) * 100
     document.getElementById("marginOfSafety").innerHTML = Math.round(marginOfSafety * 100) / 100 + "%"
-}
-
-function drawPriceOutput(responseText) {
-    let companyProfile = [JSON.parse(responseText).profile];
-    price = companyProfile[0].price
-    document.getElementById("currentPrice").innerHTML = price
-}
-function drawDCFOutput(responseText) {
-    let companyProfile = JSON.parse(responseText);
-    dcf = companyProfile[0].dcf
-    document.getElementById("DCF").innerHTML = Math.round(dcf * 100) / 100
-}
-
-// Get URL Request from Financial Modelling Prep
-function getRequest(url, success) {
-    var req = false;
-    try {
-    req = new XMLHttpRequest();
-    } catch (e) {
-    try {
-        req = new ActiveXObject("Msxml2.XMLHTTP");
-    } catch (e) {
-        try {
-        req = new ActiveXObject("Microsoft.XMLHTTP");
-        } catch (e) {
-        return false;
-        }
-    }
-    }
-    if (!req) return false;
-    if (typeof success != 'function') success = function() {};
-    req.onreadystatechange = function() {
-    if (req.readyState == 4) {
-        if (req.status === 200) {
-        success(req.responseText)
-        }
-    }
-    }
-    req.open("GET", url, true);
-    req.send(null);
-    return req;
 }
